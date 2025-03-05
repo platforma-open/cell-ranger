d80a1c64849f33753947c588097cb7ec042c3860
julenmendieta <jmendieta@milaboratories.com>
2025-03-05 14:25:35 +0100

    Include default plot options

--- ui/src/pages/CellQC.vue
+++ ui/src/pages/CellQC.vue
@@ -1,13 +1,43 @@
 <script setup lang="ts">
 import { GraphMaker, GraphMakerProps } from "@milaboratories/graph-maker";
 import '@milaboratories/graph-maker/styles';
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useApp } from "../app";
+import { PColumnIdAndSpec } from "@platforma-sdk/model";
 
 const app = useApp();
+
+function getDefaultOptions(cellMetricsPfDefaults?: PColumnIdAndSpec[]) {
+  if (!cellMetricsPfDefaults) {
+    return undefined;
+  }
+
+  function getIndex(name: string, pcols: PColumnIdAndSpec[]): number {
+    return pcols.findIndex((p) => p.spec.name === name);
+  }
+
+  const defaults: GraphMakerProps['defaultOptions'] = [
+    {
+      inputName: 'y',
+      selectedSource: cellMetricsPfDefaults[getIndex('pl7.app/rna-seq/totalCounts',
+        cellMetricsPfDefaults)].spec,
+    },
+    {
+      inputName: 'primaryGrouping',
+      selectedSource: cellMetricsPfDefaults[getIndex('pl7.app/label',
+        cellMetricsPfDefaults)].spec,
+    },
+  ];
+
+  return defaults;
+}
+
+const defaultOptions = ref(getDefaultOptions(app.model.outputs.cellMetricsPfDefaults));
 
 </script>
 
 <template>
-  <GraphMaker chartType="discrete" :p-frame="app.model.outputs.cellMetricsPf" v-model="app.model.ui.graphState" />
+  <GraphMaker chartType="discrete" :p-frame="app.model.outputs.cellMetricsPf"
+    v-model="app.model.ui.graphState" :defaultOptions="defaultOptions" 
+  />
 </template>
