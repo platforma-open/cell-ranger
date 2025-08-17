af3ccf9a57c1b38d19ca02b9b85a0e57fc020929
aiturbidemil <aiturbide@milaboratories.com>
2025-08-17 18:21:26 +0200

    update sdk and minor fixes

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,27 +1,28 @@
-<script
-  setup
-  lang="ts"
->
+<script setup lang="ts">
 import { AgGridVue } from 'ag-grid-vue3';
 import {
   PlAgTextAndButtonCell,
   PlBlockPage,
   PlBtnGhost,
-  PlDropdown,
-  PlDropdownRef,
   PlMaskIcon24,
   PlSlideModal,
-  useAgGridOptions,
+  createAgGridColDef,
+  makeRowNumberColDef,
+  AgGridTheme,
+  PlAgOverlayLoading,
+  PlAgOverlayNoRows,
 } from '@platforma-sdk/ui-vue';
 
-import { plRefsEqual } from '@platforma-sdk/model';
-import type { PlRef, ProgressLogWithInfo } from '@platforma-sdk/model';
-import { computed, reactive } from 'vue';
+import type { ProgressLogWithInfo } from '@platforma-sdk/model';
+import { computed, reactive, shallowRef, watch } from 'vue';
 import { useApp } from '../app';
 import ReportPanel from './Report.vue';
 import { resultMap } from './results';
 import { parseProgress } from '../parseProgress';
-import { speciesOptions } from '../species';
+import SettingsPanel from './SettingsPanel.vue';
+import type { ColDef, GridApi, GridOptions, GridReadyEvent } from 'ag-grid-enterprise';
+import { autoSizeRowNumberColumn } from '@platforma-sdk/ui-vue';
+import { whenever } from '@vueuse/core';
 
 const app = useApp();
 
@@ -35,6 +36,25 @@ const data = reactive<{
   selectedSample: undefined,
 });
 
+watch(
+  () => app.model.outputs.isRunning,
+  (newVal, oldVal) => {
+    if (oldVal === false && newVal === true)
+      data.settingsOpen = false;
+    if (oldVal === true && newVal === false)
+      data.settingsOpen = true;
+  },
+);
+
+whenever(
+  () => data.settingsOpen,
+  () => (data.sampleReportOpen = false),
+);
+whenever(
+  () => data.sampleReportOpen,
+  () => (data.settingsOpen = false),
+);
+
 type Row = {
   sampleId: string;
   sampleLabel: string;
@@ -43,7 +63,8 @@ type Row = {
 
 /** Rows for ag-table */
 const results = computed<Row[] | undefined>(() => {
-  if (resultMap.value === undefined) return undefined;
+  if (resultMap.value === undefined)
+    return undefined;
   const rows = [];
   for (const id in resultMap.value) {
     rows.push({
@@ -56,59 +77,66 @@ const results = computed<Row[] | undefined>(() => {
   return rows;
 });
 
-const { gridOptions } = useAgGridOptions<Row>(({ column }) => {
-  return {
-    rowData: results.value,
-    rowNumbersColumn: true,
-    defaultColDef: {
-      suppressHeaderMenuButton: true,
-      lockPinned: true,
-      sortable: false,
-    },
-    columnDefs: [
-      {
-        colId: 'label',
-        field: 'sampleLabel',
-        headerName: 'Sample',
-        pinned: 'left',
-        lockPinned: true,
-        sortable: true,
-        cellRenderer: PlAgTextAndButtonCell,
-        cellRendererParams: {
-          invokeRowsOnDoubleClick: true,
-        },
-      },
-      column<ProgressLogWithInfo | undefined>({
-        colId: 'cellRanger',
-        field: 'cellRanger',
-        headerName: 'Cell Ranger Progress',
-        flex: 1,
-        cellStyle: {
-          '--ag-cell-horizontal-padding': '0px',
-          '--ag-cell-vertical-padding': '0px',
-        },
-        progress(cellRangerProgressLine) {
-          return parseProgress(cellRangerProgressLine);
-        },
-      }),
-    ],
-  };
-});
+const gridApi = shallowRef<GridApi>();
+const onGridReady = (params: GridReadyEvent) => {
+  gridApi.value = params.api;
+  autoSizeRowNumberColumn(params.api);
+};
 
-function setInput(inputRef?: PlRef) {
-  app.model.args.ref = inputRef;
-  if (inputRef)
-    app.model.args.title = app.model.outputs.dataOptions?.find((o) => plRefsEqual(o.ref, inputRef))?.label;
-  else
-    app.model.args.title = undefined;
-}
+const defaultColumnDef: ColDef = {
+  suppressHeaderMenuButton: true,
+  lockPinned: true,
+  sortable: false,
+};
+
+const columnDefs: ColDef<Row>[] = [
+  makeRowNumberColDef(),
+  createAgGridColDef<Row, string>({
+    colId: 'label',
+    field: 'sampleLabel',
+    headerName: 'Sample',
+    pinned: 'left',
+    lockPinned: true,
+    sortable: true,
+    cellRenderer: PlAgTextAndButtonCell,
+    cellRendererParams: {
+      invokeRowsOnDoubleClick: true,
+    },
+  }),
+  createAgGridColDef<Row, ProgressLogWithInfo | undefined>({
+    colId: 'cellRanger',
+    field: 'cellRanger',
+    headerName: 'Cell Ranger Progress',
+    flex: 1,
+    cellStyle: {
+      '--ag-cell-horizontal-padding': '0px',
+      '--ag-cell-vertical-padding': '0px',
+    },
+    progress(cellRangerProgressLine) {
+      return parseProgress(cellRangerProgressLine);
+    },
+  }),
+] as any;
+
+const gridOptions: GridOptions<Row> = {
+  getRowId: row => row.data.sampleId,
+  onRowDoubleClicked: (e) => {
+    data.selectedSample = e.data?.sampleId;
+    data.sampleReportOpen = data.selectedSample !== undefined;
+  },
+  components: {
+    PlAgTextAndButtonCell,
+  },
+};
 </script>
 
 <template>
   <PlBlockPage>
-    <template #title>Cell Ranger</template>
+    <template #title>
+      Cell Ranger
+    </template>
     <template #append>
-      <PlBtnGhost @click.stop="() => data.settingsOpen = true">
+      <PlBtnGhost @click.stop="() => (data.settingsOpen = true)">
         Settings
         <template #append>
           <PlMaskIcon24 name="settings" />
@@ -116,22 +144,41 @@ function setInput(inputRef?: PlRef) {
       </PlBtnGhost>
     </template>
 
-    <AgGridVue :style="{ height: '100%' }" v-bind="gridOptions as {}" />
+    <AgGridVue
+      :theme="AgGridTheme"
+      :style="{ height: '100%' }"
+      :rowData="results"
+      :defaultColDef="defaultColumnDef"
+      :columnDefs="columnDefs"
+      :grid-options="gridOptions as any"
+      :loadingOverlayComponentParams="{ notReady: true }"
+      :loadingOverlayComponent="PlAgOverlayLoading"
+      :noRowsOverlayComponent="PlAgOverlayNoRows"
+      @grid-ready="onGridReady"
+    />
   </PlBlockPage>
 
-  <PlSlideModal v-model="data.settingsOpen">
-    <template #title>Settings</template>
-    <PlDropdownRef
-      v-model="app.model.args.ref" :options="app.model.outputs.dataOptions" label="Select dataset"
-      clearable @update:model-value="setInput"
-    />
-    <PlDropdown v-model="app.model.args.species" :options="speciesOptions" label="Select species" />
+  <PlSlideModal
+    v-model="data.settingsOpen"
+    :shadow="true"
+    :close-on-outside-click="!app.model.outputs.isRunning"
+  >
+    <template #title>
+      Settings
+    </template>
+    <SettingsPanel />
   </PlSlideModal>
 
-  <PlSlideModal v-model="data.sampleReportOpen" width="95%">
+  <PlSlideModal
+    v-model="data.sampleReportOpen"
+    :close-on-outside-click="!app.model.outputs.isRunning"
+    width="95%"
+  >
     <template #title>
-      Results for {{ (data.selectedSample ? app.model.outputs.labels?.[data.selectedSample] :
-        undefined) ?? "..." }}
+      Results for
+      {{
+        (data.selectedSample ? app.model.outputs.labels?.[data.selectedSample] : undefined) ?? '...'
+      }}
     </template>
     <ReportPanel v-model="data.selectedSample" />
   </PlSlideModal>
---
+++ ui/src/pages/SettingsPanel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { PlDropdown, PlDropdownRef } from '@platforma-sdk/ui-vue';
+import { useApp } from '../app';
+import { speciesOptions } from '../species';
+import type { PlRef } from '@platforma-sdk/model';
+import { plRefsEqual } from '@platforma-sdk/model';
+
+const app = useApp();
+
+function setInput(inputRef?: PlRef) {
+  app.model.args.ref = inputRef;
+  if (inputRef)
+    app.model.args.title = app.model.outputs.dataOptions?.find((o) => plRefsEqual(o.ref, inputRef))?.label;
+  else
+    app.model.args.title = undefined;
+}
+</script>
+
+<template>
+  <PlDropdownRef
+    v-model="app.model.args.ref"
+    :options="app.model.outputs.dataOptions"
+    label="Select dataset"
+    clearable
+    @update:model-value="setInput"
+  />
+  <PlDropdown
+    v-model="app.model.args.species"
+    :options="speciesOptions"
+    label="Select species"
+  />
+</template> 
